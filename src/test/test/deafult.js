ecc3288580e1f049548327b3abaeae5147297d51
YunKeon112 <kyun@pentasecurity.com>
2021-12-31 04:04:51 +0900

    add creatRow function

--- src/test/test/deafult.js
+++ src/test/test/deafult.js
@@ -15,7 +15,14 @@ $(document).ready(function(){
         "initComplete": function () {
             this.api().columns().every( function () {
                 var column = this;
+
+                if(column[0][0] == 0) return "&nbsp";
+				if(column[0][0] == 1) return "&nbsp";
             } );
         },
+        "createdRow": function (row, data, index){
+			$("td", row).eq(0).attr("pk", data[0]);
+		}
     });
+    
 });
