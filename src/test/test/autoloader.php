d8a3d49610beaffd6e2b596146a98a84f03a3444
YunKeon112 <kyun@pentasecurity.com>
2021-12-31 04:09:28 +0900

    add define variable

--- src/test/test/autoloader.php
+++ src/test/test/autoloader.php
@@ -5,3 +5,8 @@
 session_start();
 
 header("Content-Type: text/html; charset=UTF-8");
+
+ini_set("date.timezone", "Asia/Seoul");
+
+define ('DS', DIRECTORY_SEPARATOR);
+define ('ROOT_PATH', __DIR__ . DS);
